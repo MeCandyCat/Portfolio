da414ab2d0e1d2feb2ac5b81c0361bc3186bb14a
MeCandyCat <mecandycat@gmail.com>
2024-05-09 16:36:35 +0530

    Projects loading animation.

--- src/lib/components/project.svelte
+++ src/lib/components/project.svelte
@@ -48,8 +48,27 @@
 <Card.Root class="w-[500px]">
 	<Card.Header class="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
 		<div class="space-y-1">
-			<Card.Title>{repoDetails.name}</Card.Title>
-			<Card.Description>{repoDetails.description}</Card.Description>
+			<Card.Title>
+				{#if repoDetails.name}
+					{repoDetails.name}
+				{:else}
+				<div class="h-3 animate-pulse bg-gray-300 rounded-full dark:bg-gray-600 w-32"/>
+				{/if}
+
+			</Card.Title>
+			<Card.Description>
+				{#if repoDetails.description}
+					{repoDetails.description}
+				{:else}
+					<div role="status" class="space-y-3 animate-pulse max-w-lg">
+						<div class="flex items-center w-full">
+							<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+							<div class="h-3 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+							<div class="h-3 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+						</div>
+					</div>
+				{/if}
+			</Card.Description>
 		</div>
 		<Button
 			variant="secondary"
@@ -62,20 +81,24 @@
 		</Button>
 	</Card.Header>
 	<Card.Content>
-		<div class="flex space-x-4 text-sm text-muted-foreground">
+		<div class="flex space-x-4 text-sm text-muted-foreground items-center">
 			<div class="flex items-center">
 				<Circle class="mr-1 h-3 w-3 text-sky-400" />
-				{repoDetails.language}
+				{#if repoDetails.language}
+					<p>{repoDetails.language}</p>
+				{:else}
+					<div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-24 animate-pulse " />
+				{/if}
 			</div>
 			<div class="flex items-center">
 				<Star class="mr-1 h-3 w-3" />
 				{repoDetails.stars} stars
 			</div>
-			<div>
+			<div class="flex items-center">
 				{#if repoDetails.lastUpdatedDate}
 					<p>Updated: {repoDetails.lastUpdatedDate}</p>
 				{:else}
-					<p>Updated: <span class="text-slate-500">Loading...</span></p>
+					<p>Updated: <span class="text-slate-500 animate-pulse">Loading...</span></p>
 				{/if}
 			</div>
 		</div>
